23c1f037892b5c5d108f2fda69a9d2dcf36cdde1
AButsai <butsai@ukr.net>
2022-06-20 11:16:30 +0300

    hw-06-phonebook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react';
-import { useLocalStoreg } from 'hooks/useLocalStorage';
-import { nanoid } from 'nanoid';
-import { LOCALSTOREGE_KEY } from 'helpers/variablesHelper';
+import React from 'react';
 
 import ContactForm from '../ContactForm';
 import ContactList from '../ContactList';
@@ -9,54 +6,16 @@ import Filter from '../Filter';
 
 import s from './App.module.css';
 
-export function App() {
-  const [contact, setContact] = useLocalStoreg(LOCALSTOREGE_KEY, []);
-  const [filter, setFilter] = useState('');
-
-  const onSubmit = data => {
-    for (const { name } of contact) {
-      if (name.toLowerCase() === data.name.toLowerCase()) {
-        alert(`${name} is already in contacts.`);
-        return;
-      }
-    }
-
-    const contactId = nanoid();
-    data.id = contactId;
-
-    setContact([...contact, data]);
-  };
-
-  const onFilterName = e => {
-    const { value } = e.currentTarget;
-    setFilter(value);
-  };
-
-  const onFilterContacts = () => {
-    return [...contact].filter(
-      ({ name }) => name.toLowerCase().indexOf(filter.toLowerCase()) !== -1
-    );
-  };
-
-  const onDeleteContact = contactId => {
-    setContact([...contact].filter(({ id }) => id !== contactId));
-    setFilter('');
-  };
-
+function App() {
   return (
     <div className={s.container}>
       <h1 className={s.title}>Phonebook</h1>
-
-      <ContactForm addContacts={onSubmit} />
-
+      <ContactForm />
       <h2 className={s.contactsTitle}>Contacts</h2>
-
-      <Filter value={filter} onFilterName={onFilterName} />
-
-      <ContactList
-        filterContact={onFilterContacts()}
-        deleteContact={onDeleteContact}
-      />
+      <Filter />
+      <ContactList />
     </div>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+import App from 'components/App/App';
+import store from 'redux/store';
+import Loader from 'components/Loader';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store.store}>
+      <PersistGate loading={<Loader />} persistor={store.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
